7fa5c64469e37d91b49ed58808935b33e6f9f3ba
Valdeir S <valdeirpsr@hotmail.com.br>
2023-03-28 17:59:56 -0300

    feat(component): adiciona o componente PsrButton
    Signed-off-by: Valdeir S <valdeirpsr@hotmail.com.br>

---
+++ src/renderer/src/components/PsrButton.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let text = "Download";
+</script>
+
+<button
+  type="button"
+  class="
+  bg-[#0F2027] border-[#2C5364] border-2 rounded-2xl py-3 px-16 font-bold transition uppercase
+  hover:bg-[#5324C1] hover:border-[#FFFFFF] hover:text-white
+  "
+  on:click
+>{text}</button>
